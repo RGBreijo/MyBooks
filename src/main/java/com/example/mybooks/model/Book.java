f11dc003a2bd9908f43f2a2e7fab1803d0281f13
Raul Garcia Breijo <raulgarcia10000@gmail.com>
2021-07-02 11:51:06 -0400

    tostring

--- src/main/java/com/example/mybooks/model/Book.java
+++ src/main/java/com/example/mybooks/model/Book.java
@@ -1,6 +1,7 @@
 package com.example.mybooks.model;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 public class Book
@@ -52,6 +53,29 @@ public class Book
     }
 
 
+    @Override
+    public String toString()
+    {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", notes='" + notes + '\'' +
+                '}';
+    }
 
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(id, book.id) && Objects.equals(title, book.title) && Objects.equals(author, book.author) && Objects.equals(notes, book.notes) && Objects.equals(user, book.user);
+    }
 
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash(id, title, author, notes, user);
+    }
 }
