6bb114ce57d52a117e1d4d1effb58b104943fff9
Raul Garcia Breijo <raulgarcia10000@gmail.com>
2021-07-02 21:20:18 -0400

    A new hope

--- src/main/java/com/example/mybooks/MyBooksApplication.java
+++ src/main/java/com/example/mybooks/MyBooksApplication.java
@@ -4,15 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class MyBooksApplication
 {
-    @Bean
-    public RestTemplate getRestTemplate()
-    {
-        return new RestTemplate();
-    }
     public static void main(String[] args)
     {
         SpringApplication.run(MyBooksApplication.class, args);
--- src/main/java/com/example/mybooks/service/OpenLibService.java
+++ src/main/java/com/example/mybooks/service/OpenLibService.java
@@ -1,22 +1,53 @@
 package com.example.mybooks.service;
 
+import org.apache.tomcat.util.json.JSONParser;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
 
 @Service
 public class OpenLibService
 {
-    @Autowired
-    private RestTemplate restTemplate;
+
 
     public void test()
     {
-        String uri = "https://openlibrary.org/search.json?q=the+lord+of+the+rings";
 
-        RestTemplate restTemplate = new RestTemplate();
-        String result = restTemplate.getForObject(uri, String.class);
-         System.out.println(result);
 
+
+        String test = "https://www.googleapis.com/books/v1/volumes?q=time&printType=magazines&key=AIzaSyAIHh_8U23Sgm_Ac6o1gLHoSEoEkab4Koc";
+        String bookName = "Harry Potter and the Half-Blood Prince";
+
+        final String API_KEY = "AIzaSyAIHh_8U23Sgm_Ac6o1gLHoSEoEkab4Koc";
+
+        String URI = "https://www.googleapis.com/books/v1/volumes?q=\"" + bookName +
+                     "”&printType=books&key=" + API_KEY;
+
+
+        WebClient webClient = WebClient.create();
+
+        Mono<String> what = webClient.get().uri(URI).retrieve().bodyToMono(String.class);
+
+        try
+        {
+            JSONObject obj = new JSONObject(what.block());
+
+            JSONObject ay = new JSONObject(obj.getJSONArray("items").get(0).toString());
+
+            System.out.println(ay.getJSONObject("volumeInfo").getJSONArray("authors"));
+            System.out.println(ay.getJSONObject("volumeInfo").get("description"));
+
+
+        }catch (JSONException e)
+        {
+
+        }
+
+//        System.out.println(what.block());
     }
 }
