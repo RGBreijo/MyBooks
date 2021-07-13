636e47ad5ddafd86723b43d5b919758f84d458cc
Jonathon <Jonatan9102@gmail.com>
2021-07-13 15:12:25 -0400

    Fixed bug where UserIDs would increment when a bug was added resulting in users skipping IDs

--- src/main/java/com/example/mybooks/model/Book.java
+++ src/main/java/com/example/mybooks/model/Book.java
@@ -7,7 +7,7 @@ import java.util.Objects;
 public class Book
 {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     private String title;
--- src/main/java/com/example/mybooks/model/User.java
+++ src/main/java/com/example/mybooks/model/User.java
@@ -8,7 +8,7 @@ import java.util.Objects;
 public class User
 {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
 
@@ -62,7 +62,9 @@ public class User
     public boolean equals(Object o)
     {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (o == null || getClass() != o.getClass())
+            return false;
+
         User user = (User) o;
         return Objects.equals(id, user.id) && Objects.equals(username, user.username) && Objects.equals(books, user.books);
     }
